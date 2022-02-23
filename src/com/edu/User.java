310b61097a54a8e64708c50d0b023315dc171f34
estudiante <estudiante@01DAW01>
2022-02-23 13:44:10 +0100

    Añado los atributos, los getters y setters.

--- src/com/edu/User.java
+++ src/com/edu/User.java
@@ -2,4 +2,23 @@ package com.edu;
 
 public class User {
 
+	private String login;
+	private String password;
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
 }
